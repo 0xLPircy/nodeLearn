89912e0337f99803c6675c3bce2a12a71c36a67b
L-Pircy <taveesha.agarwal@gmail.com>
2023-06-30 12:18:16 +0530

    fs mopdule, create fole folder write, read, rename

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,8 +1,31 @@
 const fs = require('fs');
 const path = require('path');
 
-// create folder
+// create folder ((asyync))
 fs.mkdir(path.join(__dirname, '/test'), {}, err => {
     if (err) throw err;
     console.log('Folder Created...');
+});
+
+// Create and write to file
+fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
+    if (err) throw err;
+    console.log('Folder Written to...');
+    fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), 'Hello World!', err => {
+        if (err) throw err;
+        console.log('Folder Written to...');
+
+    });
+});
+
+// Reade file, utf8 converts raw buffer to actual data
+fs.readFile(path.join(__dirname, '/test', 'hello.txt'), 'utf8', (err, data) => {
+    if (err) throw err;
+    console.log(data);
+});
+
+// rename file, 2nd param is new name
+fs.rename(path.join(__dirname, '/test', 'hello.txt'), path.join(__dirname, '/test', 'helloworld.txt'), (err) => {
+    if (err) throw err;
+    console.log('file renamed');
 });

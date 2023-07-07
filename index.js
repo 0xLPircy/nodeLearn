cfadb1b9f65dd6e507c35a5108586c79229bac6d
L-Pircy <taveesha.agarwal@gmail.com>
2023-07-07 17:52:57 +0530

    creating server on index

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const Logger = require('./logger')
+// const Logger = require('./logger');
 
-const logger = new Logger();
+// const logger = new Logger();
 
-logger.on('message', (data) => console.log('called listener:', data))
+// logger.on('message', (data) => console.log('called listener:', data))
 
-logger.log('hello world');
+// logger.log('hello world');
+
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    // console.log(req.url)
+    if (req.url === '/') {
+
+    }
+});
+
+const PORT = process.env.PORT || 5001;
+
+server.listen(PORT, () => console.log(`server running on port ${PORT}`))

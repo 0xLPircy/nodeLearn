9d4fe447f92c5600dcd5cf57c32bc7b216b874d8
L-Pircy <taveesha.agarwal@gmail.com>
2023-08-06 11:15:45 +0530

    requesting url

--- index.js
+++ index.js
@@ -6,17 +6,24 @@
 
 // logger.log('hello world');
 
-const http = require('http');
-const path = require('path');
-const fs = require('fs');
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-    // console.log(req.url)
-    if (req.url === '/') {
-        res.end('<h1> Home </h1>')
-    }
+  // console.log(req.url)
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 });
 
 const PORT = process.env.PORT || 5001;
 
-server.listen(PORT, () => console.log(`server running on port ${PORT}`))
+server.listen(PORT, () => console.log(`server running on port ${PORT}`));
